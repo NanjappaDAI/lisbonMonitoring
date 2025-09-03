50002fa8edcb5df6599d5e35f3956ef2a47b034c
nanjappa <csnanjappa@yahoo.com>
2025-09-03 09:39:09 +0300

    Adhoc run

--- failedtestcases.sh
+++ failedtestcases.sh
@@ -2,7 +2,8 @@
 baseURL=http://192.168.1.213:8090/app
 accessKey=eyJ0eXAiOiAiVENWMiJ9.QksxMjlNWjFJLXhzY3JsUE5tMzROMGNuODk4.ZmQ2ZDkyNjYtMDUzYi00ZGFiLTg0ZTEtMjQyMDMzODE2ODEz
 basicAuth=bmFuamFwcGEuc29tYWlhaDoxMjM0NTY
-buildDetails=$(curl -s GET 'http://192.168.1.213:8090/app/rest/buildTypes/id:Automation_AutomationCloudExecution_ContinuousTestingMaster/builds/count:1' -H "Authorization: Bearer $accessKey")
+#buildDetails=$(curl -s GET 'http://192.168.1.213:8090/app/rest/buildTypes/id:Automation_AutomationCloudExecution_ContinuousTestingMaster/builds/count:1' -H "Authorization: Bearer $accessKey")
+buildDetails=$(curl -s GET 'http://192.168.1.213:8090/app/rest/buildTypes/id:Automation_AutomationCloudExecution_ContinuousTestingMaster/builds/id:1783544' -H "Authorization: Bearer $accessKey")
 IFS='"' read -ra buildDetailsSplit <<<"$buildDetails"
 buildID=${buildDetailsSplit[7]}
 buildNumber=${buildDetailsSplit[11]}

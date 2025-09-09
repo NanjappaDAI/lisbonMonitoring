6aefe0c9e97997ecb9965f9e176d0f42b1ff4204
nanjappa <csnanjappa@yahoo.com>
2025-09-09 17:26:23 +0300

    Averages and status

--- teamcitySI.sh
+++ teamcitySI.sh
@@ -3,7 +3,7 @@ baseURL=http://192.168.1.213:8090/app
 accessKey=eyJ0eXAiOiAiVENWMiJ9.QksxMjlNWjFJLXhzY3JsUE5tMzROMGNuODk4.ZmQ2ZDkyNjYtMDUzYi00ZGFiLTg0ZTEtMjQyMDMzODE2ODEz
 
 siStatus=$(curl -s "http://192.168.1.213:8090/app/rest/buildTypes/id:Automation_AutomationSeleniumAgent_SeleniumIntegrationTest_RunTestsSeleniumIntegrationTestsMaster/builds/count:1" -H "Authorization: Bearer $accessKey" | sed -n 's:.*<statusText>\(.*\)</statusText>.*:\1:p')
-siStatus=$(curl -s "http://192.168.1.213:8090/app/rest/buildTypes/id:Automation_AutomationSeleniumAgent_SeleniumIntegrationTest_RunTestsSeleniumIntegrationTestsMaster/builds/count:1" -H "Authorization: Bearer $accessKey" | sed -n 's:.*<startDate>\([0-9]\{8\}\).*<\/startDate>.*:\1:p')
+siBuildDate=$(curl -s "http://192.168.1.213:8090/app/rest/buildTypes/id:Automation_AutomationSeleniumAgent_SeleniumIntegrationTest_RunTestsSeleniumIntegrationTestsMaster/builds/count:1" -H "Authorization: Bearer $accessKey" | sed -n 's:.*<startDate>\([0-9]\{8\}\).*<\/startDate>.*:\1:p')
 
 recentruns="/Users/auto/lisbonmonitor/si.txt"
 echo "$siBuildDate $siStatus" >> $recentruns
@@ -13,5 +13,5 @@ averages=$(head -n 7 si_latest.txt | awk -F'[ ,:]+' '{for(i=1;i<=NF;i++){if($i==
 failed_avg=${averages%% *}
 passed_avg=${averages#* }
 
-echo "<tr> <td> "Selenium Intregration" </td> <td> $passed_avg </td> <td> $failed_avg </td> </tr>" >> average.txt
-echo "<tr> <td> "Selenium Intregration" </td> <td> $siBuildDate </td> <td> $siStatus </td> </tr>" >> dailyStatus.txt
+echo "<tr> <td> "Selenium Integration" </td> <td> $passed_avg </td> <td> $failed_avg </td> </tr>" >> average.txt
+echo "<tr> <td> "Selenium Integration" </td> <td> $siBuildDate </td> <td> $siStatus </td> </tr>" >> dailyStatus.txt

c35f9aaa94f8313f062fe86476b5779abcae9afa
nanjappa <csnanjappa@yahoo.com>
2026-01-28 08:53:26 +0200

    #113

--- teamcityLisbonTests.sh
+++ teamcityLisbonTests.sh
@@ -8,8 +8,8 @@ basicAuth=bmFuamFwcGEuc29tYWlhaDphdUdVQCkyMQ==
 #buildDetails=$(curl -s GET 'http://192.168.1.213:8090/app/rest/buildTypes/id:Automation_AutomationCloudExecution_ContinuousTestingMaster/builds/id:1858671' -H "Authorization: Bearer $accessKey")
 #buildDetails=$(curl -s GET 'http://192.168.1.213:8090/app/rest/buildTypes/id:Automation_AutomationCloudExecution_ContinuousTestingMaster/builds/id:1854232' -H "Authorization: Bearer $accessKey")
 #buildDetails=$(curl -s GET 'http://192.168.1.213:8090/app/rest/buildTypes/id:Automation_AutomationCloudExecution_ContinuousTestingMaster/builds/id:1855404' -H "Authorization: Bearer $accessKey")
-#buildDetails=$(curl -s GET 'http://192.168.1.213:8090/app/rest/buildTypes/id:Automation_AutomationCloudExecution_ContinuousTestingMaster/builds/id:1857113' -H "Authorization: Bearer $accessKey")
-buildDetails=$(curl -s GET 'http://192.168.1.213:8090/app/rest/buildTypes/id:Automation_AutomationCloudExecution_ContinuousTestingMaster/builds/id:1857275' -H "Authorization: Bearer $accessKey")
+buildDetails=$(curl -s GET 'http://192.168.1.213:8090/app/rest/buildTypes/id:Automation_AutomationCloudExecution_ContinuousTestingMaster/builds/id:1857113' -H "Authorization: Bearer $accessKey")
+#buildDetails=$(curl -s GET 'http://192.168.1.213:8090/app/rest/buildTypes/id:Automation_AutomationCloudExecution_ContinuousTestingMaster/builds/id:1857275' -H "Authorization: Bearer $accessKey")
 
 #IFS='"' read -ra buildDetailsSplit <<<"$buildDetails"
 #buildID=${buildDetailsSplit[7]}
